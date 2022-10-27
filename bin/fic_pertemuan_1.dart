00905029cca0f786e25a3b8cf3d8e001d25ddb4c
bahri <saiful.bahri.tl@gmail.com>
2022-10-27 19:42:21 +0700

    init push ke github

---
+++ bin/fic_pertemuan_1.dart
@@ -0,0 +1,5 @@
+import 'package:fic_pertemuan_1/fic_pertemuan_1.dart' as fic_pertemuan_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fic_pertemuan_1.calculate()}!');
+}
---
+++ test/fic_pertemuan_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:fic_pertemuan_1/fic_pertemuan_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
